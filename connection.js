f4259bea90edfd5d937c4df0d4fff5e483a1ab14
b-samsohang <bikram.samsohang@monotype.com>
2022-03-08 09:24:38 +0545

    * console error.

--- connection.js
+++ connection.js
@@ -3,18 +3,10 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 export const connectClient = async()=> {
-    dotenv.config();
+    try {
+        dotenv.config();
     const { Client } = pg;
-    
-    let user= process.env.USERNAME;
-    let password= process.env.PASSWORD;
-    let port= process.env.DBPORT;
-    let host= process.env.HOST;
-    let dbname= process.env.DATABASE;
 
-    // const connectionString=`postgres://${user}:${password}@${host}:${port}/${dbname}?ssl=true`;
-    // const client=new pg.Client(connectionString);
-    // // console.log('The connection string is: ', connectionString);
     const client = new Client({
         user: process.env.USERNAME,
         host: process.env.HOST,
@@ -35,4 +27,7 @@ export const connectClient = async()=> {
     const res = await client.query('select * from users');
     console.log(res.rows);
     // return client;
+    } catch(err) {
+        console.error('The error is: ', err);
+    }
 }
